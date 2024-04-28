011ce380e82cbfb7a5ba9907c0f6a77f3c69dc23
EsmarW <afghansfinest52@gmail.com>
2024-04-27 19:37:35 -0700

    guest login button

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_loginwidget.cpp
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_loginwidget.cpp
@@ -38,8 +38,7 @@ struct qt_meta_stringdata_CLASSLogWidgetENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSLogWidgetENDCLASS = QtMocHelpers::stringData(
     "LogWidget",
     "onLoginClicked",
-    "",
-    "onGuestLoginClicked"
+    ""
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -52,7 +51,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSLogWidgetENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,11 +59,9 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSLogWidgetENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x08,    1 /* Private */,
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+       1,    0,   20,    2, 0x08,    1 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -80,8 +77,6 @@ Q_CONSTINIT const QMetaObject LogWidget::staticMetaObject = { {
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<LogWidget, std::true_type>,
         // method 'onLoginClicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'onGuestLoginClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -94,7 +89,6 @@ void LogWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         (void)_t;
         switch (_id) {
         case 0: _t->onLoginClicked(); break;
-        case 1: _t->onGuestLoginClicked(); break;
         default: ;
         }
     }
@@ -120,13 +114,13 @@ int LogWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 1;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 1)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 1;
     }
     return _id;
 }
